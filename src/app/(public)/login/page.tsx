b827bf3f1df11a8bf31d7b70428e7693f35215e6
HyeongjinKim98 <201721157@sangmyung.kr>
2026-02-27 13:54:49 +0900

    feat : 로그인 페이지 초기 세팅

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+const oauth_url = `http://localhost:8080/oauth2/authorization`;
+const tmp_url = `http://localhost:3000/oauth/callback/accessToken=bearer&refreshToken=bearer`;
 export default function page() {
-  return <div>sdf</div>;
+  const handleLogin = (provider: "google" | "naver" | "kakao") => {
+    window.location.href = oauth_url;
+    // window.location.href = `${tmp_url}/${provider}`;
+  };
+  return (
+    <div className="flex flex-col">
+      <button onClick={() => handleLogin("google")}> 구글 로그인</button>
+      <button onClick={() => handleLogin("naver")}> 네이버 로그인</button>
+      <button onClick={() => handleLogin("kakao")}> 카카오 로그인</button>
+    </div>
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  const isPublic = pathname.startsWith("/login");
-  // login 페이지로 Redirect
-  if (!isPublic) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // const { pathname } = request.nextUrl;
+  // const isPublic = pathname.startsWith("/login");
+  // // login 페이지로 Redirect
+  // if (!isPublic) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 }
