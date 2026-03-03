db785883af50a03e9f0933b06a24162254f6deb8
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-03 15:12:11 +0900

    #1 fix : 약관 스키마 변경

--- src/schemas/termSchema.ts
+++ src/schemas/termSchema.ts
@@ -1,16 +1,7 @@
 import { z } from "zod";
 
 export const termsSchema = z.object({
-  age: z.boolean().refine((val) => val === true, {
-    message: "만 14세 이상 동의가 필요합니다.",
-  }),
-  service: z.boolean().refine((val) => val === true, {
-    message: "서비스 이용약관 동의가 필요합니다.",
-  }),
-  privacy: z.boolean().refine((val) => val === true, {
-    message: "개인정보 동의가 필요합니다.",
-  }),
-  marketing: z.boolean().optional(),
+  agreements: z.record(z.string(), z.boolean()),
 });
 
 export type TermsFormValues = {
