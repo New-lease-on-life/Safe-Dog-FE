b4baadf797dc5b4c6fe52691f3105cfa99ac57b5
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-10 21:53:12 +0900

    #1 fix : register로 도메인 이름 변경

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,3 +1,4 @@
+import { RegisterPage } from "@/views/register/RegisterPage";
 export default function Page(){
-
+      return <RegisterPage/>
 }
---
+++ src/views/onboarding/ui/OnboardingPage.tsx
@@ -0,0 +1,9 @@
+"use client";
+export const RegisterPage=()=> {
+  return (
+    <>
+      <div className="flex flex-col w-full h-screen">
+      </div>
+    </>
+  );
+}
