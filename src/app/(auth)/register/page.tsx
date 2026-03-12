75d0bc0e8b2567899d30c733dc12d84f7bca633b
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-13 02:31:04 +0900

    #4 feat : register 경로 및 구조 설정

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,4 @@
+import { RegisterPage } from "@/views/register/ui/registerPage";
+export default function page() {
+  return <RegisterPage />;
+}
---
+++ src/views/register/ui/registerPage.tsx
@@ -0,0 +1,4 @@
+import { CommonLayout } from "@/widgets/CommonLayout";
+export const RegisterPage = () => {
+  return <CommonLayout>hi</CommonLayout>;
+};
