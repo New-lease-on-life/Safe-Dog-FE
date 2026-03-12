536ff88dd680ac3acef79b8821e466d2b5e8a017
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-12 19:13:44 +0900

    #3 feat : 상대날짜 표시 함수

--- src/shared/lib/date.ts
+++ src/shared/lib/date.ts
@@ -10,3 +10,20 @@ export const formatDateToKR = (dateString : string)=>{
       const {year, month, day} = formatDate(dateString);
       return `${year}년 ${month}월 ${day}일`
 }
+
+export const getRelativTime = (dateString : string)=>{
+      const target = new Date(dateString);
+      const now = new Date();
+
+      const diffMs = now.getTime() - target.getTime();
+      const diffMin = Math.floor(diffMs / (1000 * 60));
+      const diffHour = Math.floor(diffMs / (1000 * 60 * 60));
+      const diffDay = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+      if(diffMin < 1) return `방금 전`;
+      if (diffMin < 60) return `${diffMin}분 전`;
+      if (diffHour < 24) return `${diffHour}시간 전`;
+      if (diffDay < 30) return `${diffDay}일 전`;
+
+      return formatDateToKR(dateString);
+}
