8692a7c916624b4192bf60e587de4758f4065d87
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-17 14:15:19 +0900

    #5 fix : header component 내부 배치 수정

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from "react";
 
-interface IHeaderProps{
-      left? : ReactNode;
-      title? : string
-      right? : ReactNode;
+interface IHeaderProps {
+  left?: ReactNode;
+  title?: string;
+  right?: ReactNode;
 }
-export const Header =({left,title,right}:IHeaderProps)=>{
-      return(
-            <header className="flex w-full p-4 items-center justify-between sticky top-0 h-16">
-                  <div>{left}</div>
-                  <div className="text-[#444444]">{title}</div>
-                  <div>{right}</div>
-            </header>
-            
-      )
-}
+export const Header = ({ left, title, right }: IHeaderProps) => {
+  return (
+    <header className="relative flex w-full p-4 items-center justify-between sticky top-0 h-16">
+      <div>{left}</div>
+      <div className="absolute left-1/2 -translate-x-1/2 text-[#444444]">
+        {title}
+      </div>
+      <div>{right}</div>
+    </header>
+  );
+};
