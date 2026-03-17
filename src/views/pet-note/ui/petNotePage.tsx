4ffce6f8d50a4b886cc4dcf63dfadcb16f95de55
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-17 15:34:56 +0900

    #5 feat : pet 상태 표시 ui

--- src/views/pet-note/ui/petNotePage.tsx
+++ src/views/pet-note/ui/petNotePage.tsx
@@ -4,6 +4,7 @@ import { Bell } from "lucide-react";
 import { PetSelect } from "@/features/pet/ui/PetSelect";
 import { getPets } from "@/entities/pet/api/pet";
 import { getGuardians } from "@/entities/guardian/api/guardian";
+import { MessageSquareDot } from "lucide-react";
 const Notice = () => <Bell />;
 import { ManageGuardians } from "@/features/guardian/ui/manageGuardians";
 export const PetNotePage = async () => {
@@ -17,6 +18,15 @@ export const PetNotePage = async () => {
         right={<Notice />}
       />
       <ManageGuardians guardians={guardians} />
+      <div className="relative m-4 text-2xl  h-60">
+        <p>오늘의 케어가 충분해,</p>
+        <p className="font-semibold">토토가 신이 났어요!</p>
+        <div className="absolute top-4 right-2 w-10 h-10 bg-wthie border-none rounded-full flex items-center justify-center border">
+          <MessageSquareDot className="text-gray-400" />
+        </div>
+        <div className="absolute bottom-4 right-4">Image</div>
+        <div className="absolute bottom-4 right-4">Image</div>
+      </div>
     </CommonLayout>
   );
 };
