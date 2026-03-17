f895ca51502661278a8c1e19f1068625429f3713
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-18 02:57:34 +0900

    fix : build 에러

--- src/app/(public)/oauth/callback/page.tsx
+++ src/app/(public)/oauth/callback/page.tsx
@@ -1,20 +1,20 @@
 "use client";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+// import { useEffect } from "react";
+// import { useSearchParams } from "next/navigation";
 
 export default function OAuthCallback() {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const accessToken = searchParams.get("accessToken");
-    const refreshToken = searchParams.get("refreshToken");
+  // useEffect(() => {
+  //   const accessToken = searchParams.get("accessToken");
+  //   const refreshToken = searchParams.get("refreshToken");
 
-    console.log(accessToken);
-    console.log(refreshToken);
+  //   console.log(accessToken);
+  //   console.log(refreshToken);
 
-    // localStorage.setItem("accessToken", accessToken);
-    // localStorage.setItem("refreshToken", refreshToken);
-  }, []);
+  //   // localStorage.setItem("accessToken", accessToken);
+  //   // localStorage.setItem("refreshToken", refreshToken);
+  // }, []);
 
   return <div>callback</div>;
 }
