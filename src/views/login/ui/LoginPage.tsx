bd0d16224b50e0ce77e9e5ce56b75ddbe63f3950
HyeongjinKim98 <201721157@sangmyung.kr>
2026-03-24 23:02:23 +0900

    #9 design : 로그인 화면

--- src/views/login/ui/LoginPage.tsx
+++ src/views/login/ui/LoginPage.tsx
@@ -1,32 +1,30 @@
 "use client";
 import { Button } from "@/shared/ui/button";
 import { useRouter } from "next/navigation";
-import { testLogin } from "@/shared/actions/auth";
-import { testGet } from "@/shared/actions/auth";
-import { registerPet } from "@/shared/actions/pet";
-import { getMyPets } from "@/shared/actions/pet";
 import { CommonLayout } from "@/widgets/CommonLayout";
-const oauth_url = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/naver`;
+import Image from "next/image";
+
 export const LoginPage = () => {
   const router = useRouter();
-  console.log(process.env.NEXT_PUBLIC_API_URL);
-  const handleNaverLogin = () => {
-    window.location.href = oauth_url;
-  };
-  const test = async () => {
-    await testLogin("test@gmail.com");
-  };
+
   return (
     <CommonLayout>
       <div className="flex flex-col w-full h-screen gap-2 p-4 items-center justify-center">
-        <div className="w-40 h-40 bg-black rounded-full"></div>
-        <div className="whitespace-pre-line text-center font-semibold text-xl">
+        <Image
+          src="/icons/logo-icon.svg"
+          alt="지켜줄개 로고"
+          width={160}
+          height={160}
+        />
+        <div className="whitespace-pre-line text-center text-secondary-900 font-normal">
           지켜줄개로 {"\n"} 아이의 건강을 지켜주세요
         </div>
 
         <Button
-          className="w-72 rounded-full h-12 mt-12"
-          onClick={handleNaverLogin}
+          className="w-72 rounded-full h-12 mt-12 bg-primary-600 hover:bg-primary-300"
+          onClick={() => {
+            router.push("/onboarding");
+          }}
         >
           로그인
         </Button>
